0ff5d90b139bde556f30de4d3a4e93952af9b6d8
chenay <anyan.chen@technicolor.com>
2019-12-27 17:27:25 +0800

    Try to improve exit strategy of DownEnoughIn
    Jesse livermore?

--- samples/downEnoughIn.py
+++ samples/downEnoughIn.py
@@ -6,22 +6,27 @@ from pyalgotrade import plotter
 from pyalgotrade.barfeed import yahoofeed
 from pyalgotrade.stratanalyzer import sharpe
 from pyalgotrade.technical import roc
+from pyalgotrade.technical import ma
 import pandas as pd
 import datetime
+from pyalgotrade.technical import cross
 
 DEBUG = True
 
+
 class DownEnoughIn(strategy.BacktestingStrategy):
     def __init__(self, feed, instrument, window=3):
-        commission=backtesting.TradePercentage(0.0025)
-        broker = backtesting.Broker(1000000, feed,commission)
+        commission = backtesting.TradePercentage(0.0025)
+        broker = backtesting.Broker(1000000, feed, commission)
         broker.getFillStrategy().setVolumeLimit(None)
-        super(DownEnoughIn, self).__init__(feed,broker)
+        super(DownEnoughIn, self).__init__(feed, broker)
         self.__instrument = instrument
         # We'll use adjusted close values, if available, instead of regular close values.
         if feed.barsHaveAdjClose():
             self.setUseAdjustedValues(True)
-        self.__pricePCT = roc.RateOfChange(feed[instrument].getPriceDataSeries(), 1).getValues()
+        self.__priceDS = feed[instrument].getPriceDataSeries()
+        self.__refDS =  ma.SMA(feed[instrument].getPriceDataSeries(), 5)
+        self.__pricePCT = roc.RateOfChange(self.__priceDS, 1).getValues()
         self.__longPos = None
         self.__window = window
         self.__enter = False
@@ -32,7 +37,7 @@ class DownEnoughIn(strategy.BacktestingStrategy):
         if self.__longPos == position:
             self.__longPos = None
         else:
-            assert(False)
+            assert (False)
 
     def print_transactions(self, action, execInfo):
         if action == broker.Order.Action.BUY:
@@ -41,14 +46,14 @@ class DownEnoughIn(strategy.BacktestingStrategy):
         elif action == broker.Order.Action.BUY_TO_COVER:
             self.info("BUY_TO_COVER at $%.2f" % (execInfo.getPrice()) +
                       " shares: " + str((execInfo.getQuantity())) + ' on ' + execInfo.getDateTime().strftime(
-                          "%Y%m%d"))
+                "%Y%m%d"))
         elif action == broker.Order.Action.SELL:
             self.info("SELL at $%.2f" % (execInfo.getPrice()) +
                       " shares:" + str((execInfo.getQuantity())) + ' on ' + execInfo.getDateTime().strftime("%Y%m%d"))
         elif action == broker.Order.Action.SELL_SHORT:
             self.info("SELL_SHORT at $%.2f" % (execInfo.getPrice()) +
                       " shares:" + str((execInfo.getQuantity())) + ' on ' + execInfo.getDateTime().strftime(
-                          "%Y%m%d"))
+                "%Y%m%d"))
 
     def onEnterOk(self, position):
         order = position.getEntryOrder()
@@ -62,7 +67,7 @@ class DownEnoughIn(strategy.BacktestingStrategy):
         if self.__longPos == position:
             self.__longPos = None
         else:
-            assert(False)
+            assert (False)
         order = position.getExitOrder()
         execInfo = order.getExecutionInfo()
         action = order.getAction()
@@ -74,15 +79,20 @@ class DownEnoughIn(strategy.BacktestingStrategy):
         # If the exit was canceled, re-submit it.
         position.exitMarket()
 
+    def getRefSMA(self):
+        return self.__refDS
+
     def onBars(self, bars):
         # Wait for enough bars to be available for specified observations
         if len(self.__pricePCT) < (self.__window + 1):
             return
-        priceS=pd.Series(self.__pricePCT)[(-self.__window):]
+        priceS = pd.Series(self.__pricePCT)[(-self.__window):]
         # self.__pricePCT = pd.Series(feed[instrument].getPriceDataSeries().getValues()).pct_change()
         self.__downCount = priceS[priceS < 0].count()
+        self.__pdPrice = pd.Series(self.__priceDS.getValues(), self.__priceDS.getDateTimes())
         bar = bars[self.__instrument]
-        if self.__longPos is not None and bar.getDateTime() != self.__enterTime:
+        # if self.__longPos is not None and bar.getDateTime() != self.__enterTime:
+        if self.__longPos is not None and self.exitLongSignal(bar):
             self.__longPos.exitMarket()
             if DEBUG:
                 print("exit long position: ", self.__longPos.getShares(), " on ",
@@ -95,14 +105,21 @@ class DownEnoughIn(strategy.BacktestingStrategy):
                     print("enter long shares: ", shares, " on ", bar.getDateTime().strftime('%Y%m%d'),
                           ' at  price: ', str(bar.getPrice()), ' volume: ', str(bar.getVolume()))
 
-
     def enterLongSignal(self, bar):
         return self.__downCount == self.__window
 
+    def exitLongSignal(self, bar):
+        # current = bar.getPrice()
+        # pivotal = self.__pdPrice[self.__enterTime:].max()
+        # limit = (pivotal - current) / pivotal >= 0.06
+        # enter = self.__pdPrice[self.__enterTime]
+        # stop = (enter - current) / enter >= 0.03
+        # return stop | limit
+        return cross.cross_below(self.__priceDS,self.__refDS)
 
 feed = yahoofeed.Feed()
-instrument = "lpyl"
-feed.addBarsFromCSV(instrument, "lpyl.csv")
+instrument = "whole_2018"
+feed.addBarsFromCSV(instrument, instrument+".csv")
 strat = DownEnoughIn(feed, instrument, 5)
 sharpeRatioAnalyzer = sharpe.SharpeRatio()
 strat.attachAnalyzer(sharpeRatioAnalyzer)
@@ -110,7 +127,7 @@ plot = True
 
 if plot:
     plt = plotter.StrategyPlotter(strat, True, True, False)
-    # plt.getInstrumentSubplot(instrument).addDataSeries("pri", strat.getEntrySMA())
+    plt.getInstrumentSubplot(instrument).addDataSeries("pri", strat.getRefSMA())
 
 strat.run()
 print("Sharpe ratio: %.2f", '%', sharpeRatioAnalyzer.getSharpeRatio(0.05))
@@ -118,5 +135,5 @@ strat.info("Final portfolio value: $%.2f" % strat.getResult())
 from datetime import datetime
 
 if plot:
-    plt.plot(fromDateTime=datetime.strptime('2017-07-01', '%Y-%m-%d'),
-             toDateTime=datetime.strptime('2018-03-01', '%Y-%m-%d'))
+    plt.plot(fromDateTime=datetime.strptime('2018-05-01', '%Y-%m-%d'),
+             toDateTime=datetime.strptime('2018-07-01', '%Y-%m-%d'))
